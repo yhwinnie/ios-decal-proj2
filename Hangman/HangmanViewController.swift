d45222cee362d58c943296f821fc9c1e08d2d673
Winnie Wen <wiwen@berkeley.edu>
2015-10-26 22:43:53 -0700

    Almost done

---
+++ Hangman/HangmanViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Gene Yoo on 10/13/15.
+//  Copyright © 2015 cs198-ios. All rights reserved.
+//
+
+import UIKit
+
+class HangmanViewController: UIViewController {
+    
+    @IBOutlet weak var hangmanImage: UIImageView!
+    let hang = Hangman()
+    
+    @IBAction func startNewGame(sender: AnyObject) {
+        hang.start()
+        self.userGuess.text = hang.knownString
+        hangmanImage.image = UIImage(named: "hangman1.gif")
+        self.lettersGuessed.text = "Guessed: "
+    }
+    @IBOutlet weak var userGuess: UILabel!
+    @IBOutlet weak var itemText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        hang.start()
+        self.userGuess.text = hang.knownString
+        hangmanImage.image = UIImage(named: "hangman1.gif")
+        
+    }
+    
+    @IBAction func startOverGame(sender: AnyObject) {
+        hang.guessedLetters = NSMutableArray()
+        let phrase = hang.answer
+        self.lettersGuessed.text = "Guessed: "
+//        for (var i = 0; i < hang.answer!.characters.count; i += 1) {
+//            if (phrase as NSString).substringWithRange(NSMakeRange(i, 1)) == " " {
+//                hang.knownString = hang.knownString! + " "
+//            } else {
+//                hang.knownString = hang.knownString! + "_"
+//            }
+//        }
+        self.userGuess.text = hang.knownString
+        hangmanImage.image = UIImage(named: "hangman1.gif")
+        print(hang.answer)
+        
+    }
+    
+    var i = 2;
+    
+
+    @IBAction func guessButton(sender: AnyObject) {
+        if (self.itemText.text!.characters.count == 1) {
+            let result = hang.guessLetter(self.itemText.text!)
+        
+            itemText.text = ""
+        
+            if (result == false) {
+                var name = "hangman\(i).gif"
+                hangmanImage.image = UIImage(named: name)
+                i += 1
+            }
+            else {
+                self.userGuess.text = hang.knownString
+            }
+        
+            self.lettersGuessed.text = hang.guesses()
+        
+            if (hang.knownString == hang.answer!) {
+                let alert = UIAlertView()
+                alert.title = "Alert"
+                alert.message = "You Win!"
+                alert.addButtonWithTitle("New Game")
+                alert.show()
+            }
+        
+            if (hang.guessedLetters!.count == 6) {
+                let alert = UIAlertView()
+                alert.title = "Alert"
+                alert.message = "You Lost!"
+                alert.addButtonWithTitle("Start Over")
+                alert.show()
+            }
+        }
+        else {
+            let alert = UIAlertView()
+            alert.title = "Alert"
+            alert.message = "Please only enter one letter!"
+            alert.addButtonWithTitle("Try again")
+            alert.show()
+            itemText.text = ""
+        }
+    }
+    
+    @IBOutlet weak var lettersGuessed: UILabel!
+
+
+    
+    
+    @IBOutlet weak var StartGame: UIButton!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        //hang.start()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        itemText.resignFirstResponder()
+        return true
+        
+    }
+}
+
--- README.md
+++ README.md
@@ -1,37 +1,26 @@
-# Project 2: Hangman App
+# Homework 3: Hangman Prep
 
 ## Due
-Monday, October 26 at 11:59 PM
+Monday, October 19 at 11:59 PM
 
 ## Description
-In Homework 3, we gave you most of the basic logic for a functioning Hangman
-game. As well, you built UI around the game. In Project 2, you'll be putting
-everything together into a well-designed and fully-functional application.
-Note that while many of the required features below have already been implemented
-in Homework 3, some requirements have changed or been refined.
+We've explored quite a bit with iOS development, and learned how to make simple
+apps. Continuing this trend, we are eventually going to make a full-on iOS
+Hangman game for our next project. This homework assignment is dealing with some
+preliminiaries and setting up some of the first steps for creating the Hangman
+game. Namely, we will be using our newly acquired UI knowledge to create the
+View for the game.
 
 ## Instructions
 1. Fork the assignment's repository from (https://github.com/iosdecal).
 2. Clone your forked repository to your local machine (the URL should contain
    YOUR_USERNAME/ios-decal-..).
-3. **Create Hangman app**
+3. **Add to HangmanViewController.swift and Main.Storyboard**
   * Required
     * A "New Game" Button (to start a New Game)
     * A "Guess" Button (for the user to submit a Guess)
-    * A method of guessing letters
-    * A UILabel that indicates the incorrect guesses (on the same screen)
-    * A square-dimensioned UIImageView that represents the "state" of the Hangman
-    * Appropriate images for each "state" of the Hangman (we provide basic ones in /basic-hangman-img)
-    * Use constraints - any and all views must scale to different devices
-    * A working Hangman game
-    * A win state, indicated by an Alert (Pop up box)
-        * Should prevent additional guesses
-    * A fail state, indicated by an Alert (Pop up box)
-        * Should prevent additional guesses
-    * A "Start Over" button
-  * Optional
-    * A smart way for the user to guess letters (since a Textfield with keyboard entry is bad UX)
-        * Warning: Having a button for each letter is the natural choice. However, consider the
-        difficulty of setting constraints on each button. Pursue at your own risk.
-    * Customized images for each "state" of the Hangman
+    * A Textfield (where user enters a letter as a guess)
+    * A UILabel that indicates what guesses have been made
+    * A square-dimensioned UIImageView that will represent the "state" of the Hangman
+    * Any necessary constraints for all subviews
 4. Add, commit, and push your modified file(s) to your forked remote repository.
